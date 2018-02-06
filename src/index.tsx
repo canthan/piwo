3c9630538b09cb6c8b6e6910038aa93857cc149d
andrzejglobisz <andrzej_globisz@epam.com>
2018-02-06 15:49:00 +0100

    basic components setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 const rootEl = document.getElementById("root");
