db512ae5f0e5a1e3e8ef5f42438987db709fd4df
canthan <andrzej.globisz@gmail.com>
2018-07-02 15:45:26 +0200

    summary fixed

--- src/components/storage/Summary/summaryService.ts
+++ src/components/storage/Summary/summaryService.ts
@@ -5,7 +5,6 @@ export class SummaryService {
   public static createSummary(stashes: Stash[]): StashSummary[] {
     let grouppedStashes = [];
     grouppedStashes = [...this.groupStashes(stashes)];
-    console.log(grouppedStashes);
     const stashSummary: StashSummary[] = this.composeSummary(grouppedStashes);
 
     return stashSummary;
@@ -15,19 +14,23 @@ export class SummaryService {
     grouppedStashes: GrouppedStash[],
     stashSummary: StashSummary[] = []
   ) {
-    console.log(grouppedStashes)
-    grouppedStashes.forEach(stash => {
-      const summary: StashSummary = this.createSummaryForStash(stash);
-      summary.bottles.small = this.getSmallBottles(stash);
-      summary.litres = this.getLiters(stash);
+    grouppedStashes.forEach(grouppedStash => {
+      const summary: StashSummary = this.createSummaryForStash(grouppedStash);
+      summary.bottles.small = this.getSmallBottles(grouppedStash);
+      summary.litres = this.getLiters(grouppedStash);
 
       stashSummary.push(summary);
     });
     return stashSummary;
   }
 
-  private static createSummaryForStash(grouppedStash: GrouppedStash): StashSummary {
-    return new StashSummary(grouppedStash.stash_name, grouppedStash.items['b050']);
+  private static createSummaryForStash(
+    grouppedStash: GrouppedStash
+  ): StashSummary {
+    return new StashSummary(
+      grouppedStash.stash_name,
+      grouppedStash.items['b050']
+    );
   }
 
   private static getLiters(grouppedStash: GrouppedStash): number {
@@ -50,31 +53,22 @@ export class SummaryService {
 
   private static groupStashes(stashes: Stash[]): GrouppedStash[] {
     const grouppedStashes: GrouppedStash[] = [];
-    let counter = 0;
     stashes.forEach(stash => {
       grouppedStashes.find(groupped => stash.stash_name === groupped.stash_name)
         ? this.addBottles(grouppedStashes, stash)
         : this.createNewGroup(grouppedStashes, stash);
-      counter++;
     });
-    console.log(counter)
     return grouppedStashes;
   }
 
   private static addBottles(grouppedStashes: GrouppedStash[], stash: Stash) {
-    const grouppedStash = grouppedStashes.find(
-      groupped => groupped.stash_name === stash.stash_name
-    );
-    console.log(grouppedStash)
+    const grouppedStash = grouppedStashes.find(groupped => groupped.stash_name === stash.stash_name);
     Object.keys(stash.items).forEach(item => {
       const quantity = Object.keys(grouppedStash.items).find(
         grouppedItem => grouppedItem === item
       );
-      quantity
-        ? (grouppedStash.items[quantity] += stash.items[quantity])
-        : (grouppedStash.items = stash.items);
+      grouppedStash.items[quantity] += stash.items[quantity];
     });
-    // console.log(grouppedStash.items['b050'])
   }
 
   private static createNewGroup(
@@ -82,7 +76,7 @@ export class SummaryService {
     stash: Stash
   ) {
     const { stash_name, items } = stash;
-    grouppedStashes.push({ stash_name, items, crates_total: 0 });
+    grouppedStashes.push({ stash_name, items: {...items}, crates_total: 0 });
   }
 }
 
